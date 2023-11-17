e675192e4205b928b62327a884686153b947d553
Khalil Gayle <118006546+fullstackkg@users.noreply.github.com>
2023-11-17 16:24:33 -0500

    Replaced PNG images with SVG

---
+++ index.js
@@ -0,0 +1,14 @@
+// API key d3372d1b
+const searchBtn = document.querySelector("#search-btn")
+const searchResults = document.querySelector(".search-results")
+
+searchBtn.addEventListener("click", () => {
+    console.log("Button clicked")
+})
+
+// Things the API will return 
+// - Title 
+// - Year 
+// - Movie
+// - Short plot 
+// - JSON 
