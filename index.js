f44100e56e5f9b455ce1e629ab8aefb2e8d290ad
Khalil Gayle <118006546+fullstackkg@users.noreply.github.com>
2023-11-25 15:22:30 -0500

    Error Handling
    - When no results are found for a particular search, the search results div return a UI notifying the user

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// API key d3372d1b
 const searchBtn = document.querySelector("#search-btn")
 const searchResults = document.querySelector(".search-results")
 const searchInput = document.querySelector("#search-bar")
@@ -10,56 +9,81 @@ searchInput.addEventListener("keypress", (e) => {
     }
 })
 
+const movieButtons = document.getElementsByClassName("watchlist-btn")
+for (const button of movieButtons) {
+    button.addEventListener("click", (e) => {
+        console.log(e.target)
+    })
+}
+
 searchBtn.addEventListener("click", async () => {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=d3372d1b&s=${searchInput.value}&type=movie`)
-    const data = await response.json()
-    const movieDetails = await Promise.all(data.Search.map(async movie => {              
-        const extraMovieData = await moreMovieInfo(movie.imdbID)
-        return {
-            ...movie,
-            ...extraMovieData
-        }
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=d3372d1b&s=${searchInput.value}&type=movie`)
+        const data = await response.json()
         
-    }))
+        if(data.Response === "False") {
+            searchResults.innerHTML = ""
+            const h3 = document.createElement("h3")
+            const div = document.createElement("div")
+            h3.style.color = "#DFDDDD"
+            h3.innerText = "Unable to find what you’re looking for. Please try another search."
+            div.append(h3)
+            searchResults.append(div)
 
-    searchResults.innerHTML = ""
+        } else {
+            const movieDetails = await Promise.all(data.Search.map(async movie => {              
+                const extraMovieData = await moreMovieInfo(movie.imdbID)
+                return {
+                    ...movie,
+                    ...extraMovieData
+                }
+                
+            }))
 
-    for (const movie of movieDetails) {
-        const {Title, Year, Genre, Plot, Runtime, Poster} = movie
+            searchResults.innerHTML = ""
 
-        let html = ""
-        html = `
-        <div class="movie-info">
-            <img src="${Poster}" class="movie-img"> 
-            <div class="movie-metadata">
-                <div class="metadata-title-div">
-                    <h3 id="metadata-title">${Title}</h3>
-                    <span id="metadata-year" class="metadata-small-text">(${Year})</span>                
+            for (const movie of movieDetails) {
+                const {Title, Year, Genre, Plot, Runtime, Poster} = movie
+
+                let html = ""
+                html = `
+                <div class="movie-info">
+                    <img src="${Poster}" class="movie-img"> 
+                    <div class="movie-metadata">
+                        <div class="metadata-title-div">
+                            <h3 id="metadata-title">${Title}</h3>
+                            <span id="metadata-year" class="metadata-small-text">(${Year})</span>                
+                        </div>
+                        <div class="metadata-info-div">
+                            <span id="metadata-time" class="metadata-small-text">${Runtime}</span>
+                            <span id="metadata-genre"class="metadata-small-text">${Genre}</span>
+                            <button id="metadata-button" class="watchlist-btn metadata-small-text" type="button">
+                                <img src="images/add-icon.svg" alt="">
+                                Watchlist
+                            </button>
+                        </div>
+                        <p id="metadata-plot">${Plot}</p>
+                    </div> 
                 </div>
-                <div class="metadata-info-div">
-                    <span id="metadata-time" class="metadata-small-text">${Runtime}</span>
-                    <span id="metadata-genre"class="metadata-small-text">${Genre}</span>
-                    <button id="metadata-button" class="watchlist-btn metadata-small-text" type="button">
-                        <img src="images/add-icon.svg" alt="">
-                        Watchlist
-                    </button>
-                </div>
-                <p id="metadata-plot">${Plot}</p>
-            </div> 
-        </div>
-        <div class="hr"></div>
-        `
-        searchResults.innerHTML += html
-    }
+                <div class="hr"></div>
+                `
+                searchResults.innerHTML += html
+            }
 
-    const movieImages = document.getElementsByClassName("movie-img")
-    for (const image of movieImages) {
-        image.addEventListener("error", (e) => {
-            e.target.onerror = null
-            e.target.src = "images/image-unavailable.svg"
-            e.target.style.minHeight = "250px"
-        })
-    }
+            const movieImages = document.getElementsByClassName("movie-img")
+            for (const image of movieImages) {
+                image.addEventListener("error", (e) => {
+                    e.target.onerror = null
+                    e.target.src = "images/image-unavailable.svg"
+                    e.target.style.minHeight = "250px"
+                })
+            }
+
+        }
+     
+    } catch (error) {
+       console.error("Error occurred: ", error)    
+    }   
         
 })
 
