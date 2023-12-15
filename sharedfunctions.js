210088cd238ffc6e521842566d65aa5bd9efe295
Khalil Gayle <118006546+fullstackkg@users.noreply.github.com>
2023-12-14 22:51:33 -0500

    Link bug fixed

--- sharedfunctions.js
+++ sharedfunctions.js
@@ -1,66 +1,72 @@
 export function createMovieElement(movie, isAddButton = true) {
-    const {Title, Year, Genre, Plot, Runtime, Poster, imdbID} = movie
+  const { Title, Year, Genre, Plot, Runtime, Poster, imdbID } = movie
 
-    const movieDiv = document.createElement("div")
-    movieDiv.classList.add("movie-info")
-    movieDiv.setAttribute("data-movie-id", imdbID)
+  const movieDiv = document.createElement('div')
+  movieDiv.classList.add('movie-info')
+  movieDiv.setAttribute('data-movie-id', imdbID)
 
-    const img = document.createElement("img")
-    img.classList.add("movie-img")
-    img.src = Poster
-    movieDiv.append(img)
+  const a = document.createElement('a')
+  a.classList.add('movie-link')
+  a.setAttribute('href', `https://www.imdb.com/title/${imdbID}`)
 
-    const metadataDiv = document.createElement("div")
-    metadataDiv.classList.add("movie-metadata")
+  const img = document.createElement('img')
+  img.classList.add('movie-img')
+  img.src = Poster
 
-    const metadataTitleDiv = document.createElement("div")
-    metadataTitleDiv.classList.add("metadata-title-div")
-    const h3 = document.createElement("h3")
-    h3.setAttribute("id", "metadata-title")
-    h3.innerText = Title
-    const spanYear = document.createElement("span")
-    spanYear.setAttribute("id", "metadata-year")
-    spanYear.innerText = Year
-    metadataTitleDiv.append(h3)
-    metadataTitleDiv.append(spanYear)
+  a.append(img)
+  movieDiv.append(a)
 
-    const metadataInfoDiv = document.createElement("div")
-    metadataInfoDiv.classList.add("metadata-info-div")
-    const spanRuntime = document.createElement('span')
-    spanRuntime.setAttribute("id", "metadata-runtime")
-    spanRuntime.innerText = Runtime
-    const spanGenre = document.createElement("span")
-    spanGenre.setAttribute("id", "metadata-genre")
-    spanGenre.innerText = Genre
-    const button = document.createElement("button")
-    
-    if(isAddButton === true) {
-        button.setAttribute("id", "metadata-btn")
-        button.classList.add("add-btn", "add-minus-btn-styling")
-        button.innerText = "Watchlist"
-    } else {
-        button.setAttribute("id", "remove-btn")
-        button.classList.add("remove-btn", "add-minus-btn-styling")
-        button.innerText = "Remove" 
-    }
+  const metadataDiv = document.createElement('div')
+  metadataDiv.classList.add('movie-metadata')
 
-    button.setAttribute("type", "button")
-    metadataInfoDiv.append(spanRuntime)
-    metadataInfoDiv.append(spanGenre)
-    metadataInfoDiv.append(button)
+  const metadataTitleDiv = document.createElement('div')
+  metadataTitleDiv.classList.add('metadata-title-div')
+  const h3 = document.createElement('h3')
+  h3.setAttribute('id', 'metadata-title')
+  h3.innerText = Title
+  const spanYear = document.createElement('span')
+  spanYear.setAttribute('id', 'metadata-year')
+  spanYear.innerText = Year
+  metadataTitleDiv.append(h3)
+  metadataTitleDiv.append(spanYear)
 
-    const plot = document.createElement("p")
-    plot.setAttribute("id", "metadata-plot")
-    plot.innerText = Plot
-  
-    metadataDiv.append(metadataTitleDiv)
-    metadataDiv.append(metadataInfoDiv)
-    metadataDiv.append(plot)
-  
-    movieDiv.append(metadataDiv)
+  const metadataInfoDiv = document.createElement('div')
+  metadataInfoDiv.classList.add('metadata-info-div')
+  const spanRuntime = document.createElement('span')
+  spanRuntime.setAttribute('id', 'metadata-runtime')
+  spanRuntime.innerText = Runtime
+  const spanGenre = document.createElement('span')
+  spanGenre.setAttribute('id', 'metadata-genre')
+  spanGenre.innerText = Genre
+  const button = document.createElement('button')
 
-    const hrDiv = document.createElement("div")
-    hrDiv.classList.add("hr")
+  if (isAddButton === true) {
+    button.setAttribute('id', 'metadata-btn')
+    button.classList.add('add-btn', 'add-minus-btn-styling')
+    button.innerText = 'Watchlist'
+  } else {
+    button.setAttribute('id', 'remove-btn')
+    button.classList.add('remove-btn', 'add-minus-btn-styling')
+    button.innerText = 'Remove'
+  }
 
-    return [movieDiv, hrDiv]
+  button.setAttribute('type', 'button')
+  metadataInfoDiv.append(spanRuntime)
+  metadataInfoDiv.append(spanGenre)
+  metadataInfoDiv.append(button)
+
+  const plot = document.createElement('p')
+  plot.setAttribute('id', 'metadata-plot')
+  plot.innerText = Plot
+
+  metadataDiv.append(metadataTitleDiv)
+  metadataDiv.append(metadataInfoDiv)
+  metadataDiv.append(plot)
+
+  movieDiv.append(metadataDiv)
+
+  const hrDiv = document.createElement('div')
+  hrDiv.classList.add('hr')
+
+  return [movieDiv, hrDiv]
 }
